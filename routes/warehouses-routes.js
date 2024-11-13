323d39c1747e2e410c67b5fdac2f219a94b862c1
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-11-12 17:11:22 -0700

    initial setup

---
+++ routes/warehouses-routes.js
@@ -0,0 +1,5 @@
+import express from 'express';
+const router = express.Router();
+
+import * as invController from '../controllers/inventories-controller.js';
+import * as invController from '../controllers/inventories-controller.js';
