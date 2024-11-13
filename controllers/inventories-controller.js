0a8f1637d3abc1a7708c93376dba218afe02f6d5
Emily Schur <ejschur@gmail.com>
2024-11-13 10:22:29 -0800

    Add GET /api/inventories endpoint

--- controllers/inventories-controller.js
+++ controllers/inventories-controller.js
@@ -1,3 +1,14 @@
-import initKnex from 'knex';
-import configuration from '../knexfile.js';
+import initKnex from "knex";
+import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
+
+const getInventoriesList = async (_req, res) => {
+  try {
+    const data = await knex("inventories");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({ message: `Error retrieving inventories: ${error}` });
+  }
+};
+
+export { getInventoriesList };
