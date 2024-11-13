ed7bd86a95e60363a8740625605bfd276929f8bc
Maria Cilloniz <majocilloniz@gmail.com>
2024-11-13 10:13:05 -0800

    Add API to get a single warehouse

--- controllers/warehouses-controller.js
+++ controllers/warehouses-controller.js
@@ -11,4 +11,20 @@ const getWarehousesList = async (_req, res) => {
   }
 };
 
-export { getWarehousesList };
+const getWarehouseById = async (req, res) => {
+  try {
+      const warehouse = await knex("warehouses")
+          .where({ id: req.params.id })
+          .first();
+      if (!warehouse) {
+          return res.status(404).json({ message: `Warehouse with ID ${req.params.id} not found`
+          });
+      }
+      res.status(200).json(warehouse);
+  } catch (error) {
+      res.status(400).json({ message: `Unable to retrieve warehouse data: ${error.message}`,
+      });
+  }
+};
+
+export { getWarehousesList, getWarehouseById };
