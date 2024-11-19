c23eff6f78419c29b4ba4a9ae8783b26c443efcc
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-11-19 14:50:01 -0700

    setup for deploying

--- knexfile.js
+++ knexfile.js
@@ -5,12 +5,15 @@ import 'dotenv/config';
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 export default {
-  client: 'mysql2',
+  client: 'pg',
   connection: {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    charset: 'utf8',
+    port: 5432,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
   },
 };
